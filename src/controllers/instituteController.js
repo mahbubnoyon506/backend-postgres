eddb5e718bc358a66ea3867822ebb09e2174f811
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2026-02-27 19:47:27 +0600

    Institute update and delete operation handeled

--- src/controllers/instituteController.js
+++ src/controllers/instituteController.js
@@ -33,3 +33,34 @@ exports.getAllInstitutes = async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 }
+
+exports.deleteInstitute = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await Institute.destroy({ where: { id } })
+        if (deleted) {
+            return res.status(201).json({ message: "Institute deleted" })
+        }
+        throw new Error("Institute not found")
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
+
+exports.updateInstitute = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, location } = req.body;
+
+        const institute = await Institute.findByPk(id);
+        if (!institute) return res.status(404).json({ message: 'Institute not found with the ID' })
+
+        await institute.update({
+            name: name || institute.name,
+            location: location || institute.location
+        })
+        res.status(201).json({ message: "Institute updated", institute })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+}
